970f930c7718bb6e39294b51ca3afc1b167a6dba
Bernapai <juanemeunier@gmail.com>
2024-12-16 21:03:08 -0300

    decimo cambio

--- Gestor De Inventario/config.py
+++
@@ -1,5 +0,0 @@
-class Config:
-    # Configuración de la base de datos, donde 'inventario' es el nombre de tu base de datos
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:Juanber123()@localhost:3306/inventario'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False  # Deshabilitar el seguimiento de modificaciones
-    SECRET_KEY = 'Juanber123()'  
